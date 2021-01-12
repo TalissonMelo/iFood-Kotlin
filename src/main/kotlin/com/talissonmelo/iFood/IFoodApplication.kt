1074918a475908532e698e9230c8da9141dec022
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-01-12 19:55:05 -0300

    Clone iFood kotlin

---
+++ src/main/kotlin/com/talissonmelo/iFood/IFoodApplication.kt
@@ -0,0 +1,11 @@
+package com.talissonmelo.iFood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IFoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<IFoodApplication>(*args)
+}
---
+++ src/test/kotlin/com/talissonmelo/iFood/IFoodApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.talissonmelo.iFood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IFoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
